dfa010598278432c4d149cd842b73eaa4471c5c3
manas <macmanas4@gmail.com>
2018-07-28 15:23:12 +0530

    first version

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://localhost/Movie');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
@@ -20,10 +20,14 @@ var users = require('./routes/users');
 // Init App
 var app = express();
 
+
 // View Engine
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));
 app.set('view engine', 'handlebars');
+
+
 
 // BodyParser Middleware
 app.use(bodyParser.json());
@@ -46,42 +50,50 @@ app.use(passport.session());
 
 // Express Validator
 app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
 }));
 
 // Connect Flash
 app.use(flash());
 
 // Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
 });
-
 
 
 app.use('/', routes);
 app.use('/users', users);
 
+var path = "/";
+app.get('/test', (req, res) => {
+    res.render('test.ejs', { data: "colgate" });
+});
+app.post('/test', (req, res) => {
+    console.log(req.body);
+    res.send('success');
+});
+
 // Set Port
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+app.listen(app.get('port'), function() {
+    console.log('Server started on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,126 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
 
-// Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/Movie', function(err) {
+    if (!err) {
+        console.log("no error!")
+    }
+});
+var doc = mongoose.model('movies', new Schema({ name: String }));
+
+
+
+router.get('/', ensureAuthenticated, (req, res, next) => {
+    let name = req.query.search;
+    var lname = "";
+    if (name) {
+        lname = name.toLowerCase();
+        console.log("-------------------------" + lname);
+    }
+    var re = new RegExp(lname);
+    doc.find({}, function(err, collection) {
+        //console.log(collection)
+        const s_res = JSON.stringify(collection);
+        const j_res = JSON.parse(s_res);
+        var arr = [];
+        j_res.forEach(element => {
+            if (element.Name.toLowerCase().includes(lname))
+                arr.push(element);
+        });
+
+        //console.log(j_res[0].Name);
+        res.render('index.ejs', { data: arr, layout: false });
+    })
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
+router.get('/genere/:gnr', ensureAuthenticated, (req, res) => {
+    let name = req.query.search;
+    var lname = "";
+    if (name) {
+        lname = name.toLowerCase();
+        console.log("-------------------------" + lname);
+    }
+
+    let genere = req.params.gnr;
+    doc.find({ Genere: genere }, function(err, collection) {
+        const s_res = JSON.stringify(collection);
+        const j_res = JSON.parse(s_res);
+
+        var arr = [];
+        j_res.forEach(element => {
+            if (element.Name.toLowerCase().includes(lname))
+                arr.push(element);
+        });
+
+        res.render('genere.ejs', { data: arr, layout: false, gnr: genere });
+
+    })
+});
+router.get('/country/:ctr', ensureAuthenticated, (req, res) => {
+    let name = req.query.search;
+    var lname = "";
+    if (name) {
+        lname = name.toLowerCase();
+        console.log("-------------------------" + lname);
+    }
+    let country = req.params.ctr;
+    doc.find({ Country: country }, function(err, collection) {
+        const s_res = JSON.stringify(collection);
+        const j_res = JSON.parse(s_res);
+        var arr = [];
+        j_res.forEach(element => {
+            if (element.Name.toLowerCase().includes(lname))
+                arr.push(element);
+        });
+        res.render('country.ejs', { data: arr, layout: false, ctr: country });
+    })
+});
+
+router.get('/watch_movie/:path', ensureAuthenticated, (req, res) => {
+    console.log(req.user);
+    let m_path = req.params.path;
+    console.log(m_path);
+    doc.find({ Name: m_path }, function(err, collection) {
+        const s_res = JSON.stringify(collection);
+        const j_res = JSON.parse(s_res);
+        console.log(j_res[0].Comment);
+        var arr = [];
+        arr = j_res[0].Comment;
+        res.render('watch_movie.ejs', { stuff: j_res[0], comments: arr, layout: false, name: m_path });
+    });
+});
+router.post('/watch_movie/:path', ensureAuthenticated, (req, res) => {
+    let m_path = req.params.path;
+    var now = new Date().toString().substr(0, 15);
+    var query = { Name: m_path };
+    console.log(req.body.comment);
+    var update = {
+        username: req.user.username,
+        userID: req.user._id,
+        body: req.body.comment,
+        createdAt: now
+    };
+    mongoose.connection.db.collection('movies').update({ Name: m_path }, { $set: { Comment: update } },
+        function(error) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('updated');
+                res.redirect('/watch_movie/' + m_path);
+            }
+        });
+
+});
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
 }
 
 module.exports = router;
